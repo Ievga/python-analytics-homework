a6ba1730f228cd0f3a927b7327ae63bd6a1ef13b
Ievheniia <kryzhanovskayaj@gmail.com>
2026-02-28 16:53:08 +0000

    add average sales comment

--- analysis.py
+++ analysis.py
@@ -1,2 +1,6 @@
 print("Аналіз даних розпочато!")
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
+data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
+average_sales = 883.33
+print("Середнє значення:", average_sales)
+print("Це середній рівень продажів по трьох містах")
